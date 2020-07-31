3d6a4f2b87002293647432845b0471d864e2ed50
Marek Szuba <mszuba01@gmail.com>
2020-07-31 21:03:21 +0200

    initial commit

---
+++ routes/subscribers.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+// Getting all subs
+
+// Getting one sub
+// Creating one sub
+// Updating one sub
+// Deleting one sub
+
+
+module.exports = router
